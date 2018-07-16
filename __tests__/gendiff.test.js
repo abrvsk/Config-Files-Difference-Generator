9aa3ac8e294157fe7173d6d83f100bc73e44e9a7
Andrii Borovskyi <abrvsk@gmail.com>
2018-07-16 22:20:02 +0700

    updated test file

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,5 +1,10 @@
 const gendiff = require('../src/index');
 
-test('relations test', () => {
-  expect(gendiff(1)).toBe(1);
+const first = 'Compared files/before.json';
+const second = 'Compared files/after.json';
+
+const expected = '{\nhost: hexlet.io\n+ timeout: 20\n- timeout: 50\n- proxy: 123.234.53.22\n+ verbose: true\n- follow: false\n}';
+
+test('difference test', () => {
+  expect(gendiff(first, second)).toBe(expected);
 });
