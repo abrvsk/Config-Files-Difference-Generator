9b6b82d03a337660bd9073c043f0c5edf03f9509
Andrii Borovskyi <abrvsk@gmail.com>
2018-07-17 18:06:15 +0700

    added flat yaml support

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,10 +1,16 @@
 import fs from 'fs';
 import gendiff from '../src';
 
-const first = '__tests__/__fixtures__/before.json';
-const second = '__tests__/__fixtures__/after.json';
+const firstJSON = '__tests__/__fixtures__/before.json';
+const secondJSON = '__tests__/__fixtures__/after.json';
+const firstYAML = '__tests__/__fixtures__/before.yml';
+const secondYAML = '__tests__/__fixtures__/after.yml';
 const expected = fs.readFileSync('__tests__/__fixtures__/expected.md', 'UTF-8');
 
-test('difference test', () => {
-  expect(gendiff(first, second)).toEqual(expected);
+test('difference test for JSON', () => {
+  expect(gendiff(firstJSON, secondJSON)).toEqual(expected);
+});
+
+test('difference test for YAML', () => {
+  expect(gendiff(firstYAML, secondYAML)).toEqual(expected);
 });
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import fs from 'fs';
 import _ from 'lodash';
+import path from 'path';
+import yaml from 'js-yaml';
+
 
 const genDiff = (firstConfig, secondConfig) => {
   // parse files to JSON.objects
-  const firstContent = JSON.parse(fs.readFileSync(firstConfig, 'UTF-8'));
-  const secondContent = JSON.parse(fs.readFileSync(secondConfig, 'UTF-8'));
+  let firstContent;
+  let secondContent;
+
+  if (path.extname(firstConfig) === '.yml') {
+    firstContent = yaml.safeLoad(fs.readFileSync(firstConfig, 'utf8'));
+    secondContent = yaml.safeLoad(fs.readFileSync(secondConfig, 'utf8'));
+  } else {
+    firstContent = JSON.parse(fs.readFileSync(firstConfig, 'UTF-8'));
+    secondContent = JSON.parse(fs.readFileSync(secondConfig, 'UTF-8'));
+  }
 
   const allKeys = _.union(Object.keys(firstContent), Object.keys(secondContent));
 
