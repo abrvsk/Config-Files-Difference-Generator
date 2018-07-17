a35ba4bbf82debe25c23010b03f566e5eef99c1e
Andrii Borovskyi <abrvsk@gmail.com>
2018-07-17 22:52:29 +0700

    test file change

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,29 +1,33 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-const pathBuilder = str => `__tests__/__fixtures__/${str}`;
+const pathBuilder = (str) => {
+  const pathToFile = '__tests__/__fixtures__/';
+  if (str === '.md') return `${path}expected${str}`;
+  return [`${path}before${str}`, `${path}after${str}`];
+};
 
-const firstJSON = pathBuilder('before.json');
-const secondJSON = pathBuilder('after.json');
+const firstJSON = pathBuilder('.json');
+// const secondJSON = pathBuilder('after.json');
 
-const firstYAML = pathBuilder('before.yml');
-const secondYAML = pathBuilder('after.yml');
+const firstYAML = pathBuilder('.yml');
+// const secondYAML = pathBuilder('after.yml');
 
-const firstINI = pathBuilder('before.ini');
-const secondINI = pathBuilder('after.ini');
+const firstINI = pathBuilder('.ini');
+// const secondINI = pathBuilder('after.ini');
 
-const testFile = pathBuilder('expected.md');
+const testFile = pathBuilder('.md');
 
 const expected = fs.readFileSync(testFile, 'UTF-8');
 
 test('difference test for JSON', () => {
-  expect(genDiff(firstJSON, secondJSON)).toEqual(expected);
+  expect(genDiff(...firstJSON)).toEqual(expected);
 });
 
 test('difference test for YAML', () => {
-  expect(genDiff(firstYAML, secondYAML)).toEqual(expected);
+  expect(genDiff(...firstYAML)).toEqual(expected);
 });
 
 test('difference test for INI', () => {
-  expect(genDiff(firstINI, secondINI)).toEqual(expected);
+  expect(genDiff(...firstINI)).toEqual(expected);
 });
