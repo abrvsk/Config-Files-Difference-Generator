b83660cc42d05c3f8ee82a4af9f52919e18a6a54
Andrii Borovskyi <abrvsk@gmail.com>
2018-07-20 14:56:12 +0700

    fixed quotes mistake

--- src/renderers/plain.js
+++ src/renderers/plain.js
@@ -8,7 +8,7 @@ const nodeType = [
   },
   {
     type: 'changed',
-    func: (node, parent, g) => `Property ${parent}${node.name} was updated from ${g(node.oldValue)} to ${g(node.newValue)}`,
+    func: (node, parent, g) => `Property '${parent}${node.name}' was updated from ${g(node.oldValue)} to ${g(node.newValue)}`,
   },
   {
     type: 'unchanged',
@@ -16,11 +16,11 @@ const nodeType = [
   },
   {
     type: 'added',
-    func: (node, parent, g) => `Property ${parent}${node.name} was added with value: ${g(node.newValue)}`,
+    func: (node, parent, g) => `Property '${parent}${node.name}' was added with value: ${g(node.newValue)}`,
   },
   {
     type: 'deleted',
-    func: (node, parent) => `Property ${parent}${node.name} was removed`,
+    func: (node, parent) => `Property '${parent}${node.name}' was removed`,
   },
 ];
 
